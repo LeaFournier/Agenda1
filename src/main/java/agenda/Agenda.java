1a2d2716456906bea9c04669431ee3991e01c8ec
leaFournier <lea.fournier@etud.univ-jfc.fr>
2020-12-11 08:53:50 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/Classe.java

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -7,14 +7,15 @@ import java.util.*;
  * Description : An agenda that stores events
  */
 public class Agenda {
+    ArrayList <Event> mesEvents = new ArrayList <Event>();
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
     public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        this.mesEvents.add(e);
+        
     }
 
     /**
